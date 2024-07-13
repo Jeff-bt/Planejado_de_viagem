58171af21d0a007d3874d1e39f8df212688f1196
Jeff <jeffjunio.bt@gmail.com>
2024-07-13 10:14:57 -0300

    ✨ Feat: adding global button

--- frontend/src/pages/create-trip/step/invite-guests-step.tsx
+++ frontend/src/pages/create-trip/step/invite-guests-step.tsx
@@ -1,4 +1,5 @@
 import { UserRoundPlus, ArrowRight } from "lucide-react";
+import { Button } from "../../../components/button";
 
 interface IInviteGuestsStepProps {
     openGuestsModals: () => void;
@@ -15,10 +16,10 @@ export function InviteGuestsStep({openConfirmTripModal, openGuestsModals}:IInvit
 
             <div className='w-px h-6 bg-zinc-800'/>
 
-            <button onClick={openConfirmTripModal} className='bg-lime-300 text-lime-950 rounded-lg px-5 py-2 font-medium flex items-center gap-2 hover:bg-lime-400'>
+            <Button  onClick={openConfirmTripModal} variant="primary" size="default">
               Confirmar viagem
               <ArrowRight className='size-5' />
-            </button>
+            </Button>
           </div>
     )
 }
--- frontend/src/pages/trip-details/create-activity-modal.tsx
+++ frontend/src/pages/trip-details/create-activity-modal.tsx
@@ -1,4 +1,5 @@
 import { X, Tag, Calendar } from "lucide-react";
+import { Button } from "../../components/button";
 
 interface ICreateActivityModalProps {
     closeCreateActivityModal: () => void;
@@ -42,9 +43,10 @@ export function CreateActivityModal ({ closeCreateActivityModal }:ICreateActivit
                         </div>
     
                       </div>
-                      <button type='submit' className='w-full justify-center bg-lime-300 text-lime-950 rounded-lg px-5 h-11 font-medium flex items-center gap-2 hover:bg-lime-400'>
+
+                      <Button type="submit" variant="primary" size="full">
                         Salvar Atividade
-                      </button>
+                      </Button>
                   </form>
                   
                 </div>
